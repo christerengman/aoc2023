c6841f00c4a7126d6c11e27f5a69ec218530e7f5
christerengman <github@christer.se>
2024-01-02 00:20:07 +0100

    Day 1 - 2

--- day01/Day01.ps1
+++ day01/Day01.ps1
@@ -1,14 +1,42 @@
+$digits = @('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine')
+
+function Get-Digit ([string] $s, [int] $start, [int] $step) {
+  # Numerical digit
+  if ($s[$start] -match '\d') {
+    return $s[$start]
+  }
+
+  # Lexical digit
+  for ($i = 0; $i -lt $digits.Count; $i++) {
+    if ($s.Substring($start).StartsWith($digits[$i])) {
+      return "$($i + 1)"
+    }
+  }
+
+  # Recurse
+  return Get-Digit $s ($start + $step) $step
+}
+
 Get-Content "$PSScriptRoot/input.txt" |
 ForEach-Object {
-  $sum = 0
+  $sum1 = 0
+  $sum2 = 0
 } {
+  # Part 1
   if ($_ -match '^\D*(\d).*?(\d)?\D*$') {
     $first = $Matches[1]
     $last = $Matches[2] ?? $first
     $value = [int] "$first$last"
-    Write-Debug "$_ : $value"
-    $sum += $value
+    $sum1 += $value
   }
+
+  # Part 2
+  $first = Get-Digit $_ 0 1
+  $last = Get-Digit $_ ($_.Length - 1) -1
+  $value = [int] "$first$last"
+  $sum2 += $value
+
 } {
-  $sum
+  $sum1
+  $sum2
 }
